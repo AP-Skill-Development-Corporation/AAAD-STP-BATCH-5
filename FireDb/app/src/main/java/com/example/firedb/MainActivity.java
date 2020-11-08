165d2fc5e0ba1656f87f3dd2b2c09973b128c4b6
chaitanya <chaitanyakarri963@gmail.com>
2020-11-08 09:30:47 +0530

    08-11-2020

--- FireDb/app/src/main/java/com/example/firedb/MainActivity.java
+++ FireDb/app/src/main/java/com/example/firedb/MainActivity.java
@@ -1,18 +1,73 @@
 package com.example.firedb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name,roll,number;
+    RecyclerView rv;
+    DatabaseReference reference;
+    List<MyModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        roll = findViewById(R.id.roll);
+        number = findViewById(R.id.phone);
+        rv = findViewById(R.id.rv);
+        list = new ArrayList<>();
+        reference= FirebaseDatabase.getInstance().getReference("Student");
+        /*To read the data from the firebase database*/
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    MyModel myModel=dataSnapshot.getValue(MyModel.class);
+                    list.add(myModel);
+                }
+                MyAdapter adapter = new MyAdapter(MainActivity.this,list);
+                rv.setAdapter(adapter);
+                rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                rv.addItemDecoration(new DividerItemDecoration(MainActivity.this,
+                        DividerItemDecoration.VERTICAL));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Failed to fetch",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void save(View view) {
+        String sname = name.getText().toString();
+        String sroll = roll.getText().toString();
+        String snumber = number.getText().toString();
+        MyModel myModel = new MyModel(sname,sroll,snumber);
+        /*To Write the data into firebase database*/
+        /*reference.setValue(myModel)*/
+       /* reference.child(reference.push().getKey()).setValue(myModel);*/
+        reference.child(sroll).setValue(myModel);
+        Toast.makeText(this, "Inserted", Toast.LENGTH_SHORT).show();
     }
 }
--- FireDb/app/src/main/java/com/example/firedb/MyAdapter.java
+++ FireDb/app/src/main/java/com/example/firedb/MyAdapter.java
@@ -1,15 +1,23 @@
 package com.example.firedb;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
@@ -33,6 +41,53 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
         holder.tv.setText(list.get(position).getName());
         holder.tv1.setText(list.get(position).getRoll());
         holder.tv2.setText(list.get(position).getNumber());
+        holder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference reference = FirebaseDatabase
+                        .getInstance().getReference("Student");
+                /*To Delete the data from the firebase database*/
+                reference.child(list.get(position).getRoll()).removeValue();
+                Toast.makeText(ct, "Data Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ct);
+                View v = LayoutInflater.from(ct).inflate(R.layout.update,null,false);
+                final EditText uname = v.findViewById(R.id.uname);
+                final EditText uroll = v.findViewById(R.id.uroll);
+                final EditText unumber = v.findViewById(R.id.unumber);
+                builder.setView(v);
+                builder.setCancelable(false);
+
+                uname.setText(list.get(position).getName());
+                uroll.setText(list.get(position).getRoll());
+                uroll.setEnabled(false);
+                unumber.setText(list.get(position).getNumber());
+
+                builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Student");
+                        HashMap<String,Object> map = new HashMap<>();
+                        map.put("name",uname.getText().toString());
+                        map.put("number",unumber.getText().toString());
+                        /*To update the data in the firebase Database*/
+                        reference.child(list.get(position).getRoll()).updateChildren(map);
+                        Toast.makeText(ct, "Data Updated", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
     @Override
